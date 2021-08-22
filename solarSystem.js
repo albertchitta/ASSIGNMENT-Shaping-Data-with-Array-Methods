6a9e973c09cd1e5e5de11219095be57d5e2bc2d4
albertchitta <albert.w.chittaphong@gmail.com>
2021-08-21 19:35:36 -0500

    Setup Solar System practice

---
+++ solarSystem.js
@@ -0,0 +1,46 @@
+const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+/*
+    Use the forEach method to add the name of each planet
+    to a section element in your HTML with an id of "planets".
+    Use string templates to construct the DOM elements.
+*/
+
+// Render to DOM
+const renderToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const planetEl = document.getElementById("planets")
+
+manufacturingBusinesses.forEach(business => {
+  const zipCode = business.addressZipCode;
+  outEl.innerHTML += `
+    <h2>${business.companyName}</h2>
+    <section>
+      ${business.addressFullStreet}
+    </section>
+    <section>
+      ${business.addressCity}, ${business['addressStateCode']} ${zipCode}
+    </section>
+  `
+  outEl.innerHTML += "<hr/>"
+});
+
+/*
+    Use the map method to create a new array where the
+    first letter of each planet is capitalized. Use the
+    `toUpperCase()` method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+*/
+
+
+/*
+    Use the filter method to create a new array that
+    contains planets with the letter 'e'. Use the `includes()`
+    method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+*/
